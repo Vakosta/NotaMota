30f9b635902b0c84b58ed950474ad085367f9b1c
ZZooRM <zzoorm@ya.ru>
2018-03-30 23:40:32 +0300

    Add get_actions method

--- recommendations/transformer.py
+++ recommendations/transformer.py
@@ -14,8 +14,8 @@ class Normalizer(object):
         data_tmp = data.copy()
         data_tmp['rating'] = (
             data_tmp
-            .groupby(self.__object_type__)['rating']
-            .transform(
+                .groupby(self.__object_type__)['rating']
+                .transform(
                 lambda x: (x - x.mean()) / x.std()
                 if x.std() > 0
                 else (x - self.__total_mean__) / self.__total_std__
